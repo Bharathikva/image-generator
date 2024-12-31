720ce8d2fa70b3c2a0ee8b0a0226b8e1737e9444
Bharathikva <sundarabharathi996@gmail.com>
2024-12-31 23:27:48 +0530

    adsense

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ImageGeneratorComponent } from "./components/image-generator/image-generator.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ ImageGeneratorComponent],
+  imports: [ImageGeneratorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'image-generator';
+  ngAfterViewInit(): void {
+    try {
+      (window as any).adsbygoogle = (window as any).adsbygoogle || [];
+      (window as any).adsbygoogle.push({});
+    } catch (e) {
+      console.error('AdSense error:', e);
+    }
+  }
 }
