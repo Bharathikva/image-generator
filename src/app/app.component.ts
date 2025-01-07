bf2ca19ce62c590f62ea548862f8af1501f13060
Bharathikva <sundarabharathi996@gmail.com>
2025-01-07 22:05:36 +0530

    add all content pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ImageGeneratorComponent } from "./components/image-generator/image-generator.component";
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ImageGeneratorComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
